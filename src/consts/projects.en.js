90091b1282077f7643a4d18201089edb263a88fc
Juan Camilo Ramirez T <juancaram21@gmail.com>
2026-01-17 20:55:46 -0500

    feat: minor changes

--- src/consts/projects.en.js
+++ src/consts/projects.en.js
@@ -1,37 +1,37 @@
 export const projects = [
   {
     name: "Integra",
-    description: "Graphical interface for asset inventory management of Procytec SAS company",
+    description: "Graphical interface for asset inventory management of Procytec SAS company. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Graphical interface for asset inventory management of Procytec SAS company",
+    description: "Graphical interface for asset inventory management of Procytec SAS company. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Graphical interface for asset inventory management of Procytec SAS company",
+    description: "Graphical interface for asset inventory management of Procytec SAS company. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Graphical interface for asset inventory management of Procytec SAS company",
+    description: "Graphical interface for asset inventory management of Procytec SAS company. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Graphical interface for asset inventory management of Procytec SAS company",
+    description: "Graphical interface for asset inventory management of Procytec SAS company. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Graphical interface for asset inventory management of Procytec SAS company",
+    description: "Graphical interface for asset inventory management of Procytec SAS company. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   }
--- src/consts/projects.es.js
+++ src/consts/projects.es.js
@@ -1,37 +1,37 @@
 export const projects = [
   {
     name: "Integra",
-    description: "Interfaz grafica para la gestión de inventarios de activos de la empresa Procytec SAS",
+    description: "Interfaz grafica para la gestión de inventarios de activos de la empresa Procytec SAS. Este proyecto está aún en desarrollo y tiene restricciones de privacidad. En pocas palabras, no puedo proporcionar el link de la página, solo puedo compartir imágenes.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Interfaz grafica para la gestión de inventarios de activos de la empresa Procytec SAS",
+    description: "Interfaz grafica para la gestión de inventarios de activos de la empresa Procytec SAS. Este proyecto está aún en desarrollo y tiene restricciones de privacidad. En pocas palabras, no puedo proporcionar el link de la página, solo puedo compartir imágenes.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Interfaz grafica para la gestión de inventarios de activos de la empresa Procytec SAS",
+    description: "Interfaz grafica para la gestión de inventarios de activos de la empresa Procytec SAS. Este proyecto está aún en desarrollo y tiene restricciones de privacidad. En pocas palabras, no puedo proporcionar el link de la página, solo puedo compartir imágenes.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Interfaz grafica para la gestión de inventarios de activos de la empresa Procytec SAS",
+    description: "Interfaz grafica para la gestión de inventarios de activos de la empresa Procytec SAS. Este proyecto está aún en desarrollo y tiene restricciones de privacidad. En pocas palabras, no puedo proporcionar el link de la página, solo puedo compartir imágenes.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Interfaz grafica para la gestión de inventarios de activos de la empresa Procytec SAS",
+    description: "Interfaz grafica para la gestión de inventarios de activos de la empresa Procytec SAS. Este proyecto está aún en desarrollo y tiene restricciones de privacidad. En pocas palabras, no puedo proporcionar el link de la página, solo puedo compartir imágenes.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Interfaz grafica para la gestión de inventarios de activos de la empresa Procytec SAS",
+    description: "Interfaz grafica para la gestión de inventarios de activos de la empresa Procytec SAS. Este proyecto está aún en desarrollo y tiene restricciones de privacidad. En pocas palabras, no puedo proporcionar el link de la página, solo puedo compartir imágenes.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   }

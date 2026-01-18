d52c2437e88f5100ca578b413f6f8bcc62085e09
Juan Camilo Ramirez T <juancaram21@gmail.com>
2026-01-18 00:17:00 -0500

    refactor: change the projects description

--- src/consts/projects.en.js
+++ src/consts/projects.en.js
@@ -1,37 +1,37 @@
 export const projects = [
   {
     name: "Integra",
-    description: "Graphical interface for asset inventory management of Procytec SAS company. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
+    description: "Integra is an asset inventory dashboard for Procytec company, allowing the management and creation of their assets. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Graphical interface for asset inventory management of Procytec SAS company. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
+    description: "Integra is an asset inventory dashboard for Procytec company, allowing the management and creation of their assets. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Graphical interface for asset inventory management of Procytec SAS company. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
+    description: "Integra is an asset inventory dashboard for Procytec company, allowing the management and creation of their assets. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Graphical interface for asset inventory management of Procytec SAS company. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
+    description: "Integra is an asset inventory dashboard for Procytec company, allowing the management and creation of their assets. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Graphical interface for asset inventory management of Procytec SAS company. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
+    description: "Integra is an asset inventory dashboard for Procytec company, allowing the management and creation of their assets. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   },
   {
     name: "Integra",
-    description: "Graphical interface for asset inventory management of Procytec SAS company. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
+    description: "Integra is an asset inventory dashboard for Procytec company, allowing the management and creation of their assets. This project is still in development and has privacy restrictions. In short, I cannot provide the link to the page, I can only share images.",
     state: "Development",
     image_url: "/images/projects/stage-integra.png"
   }
